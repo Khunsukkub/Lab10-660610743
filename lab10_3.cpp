8b028ff944d5fc4b586230b121007e0a81919c84
khun <khun@hotmail.com>
2023-12-19 16:31:12 +0700

    Updated

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    ifstream score;
+    score.open("score.txt");
+
+    int i = 0;
+    double num , sum , xbar , stdn , snum;
+
+    while(score >> num){
+        sum += num;
+        i++;
+    }
+
+    xbar = sum / ( i ) ;
+    score.clear();
+    score.seekg(0, ios::beg);
+
+    while(score >> num){
+        snum += pow(num - xbar, 2);
+    }
+
+    stdn = sqrt(snum / (i));
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< xbar << endl;
+    cout << "Standard deviation = " << stdn;
+}
